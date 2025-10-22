a0ce1aba9f057ff73bca374db5d58897092517ba
Hussein Saleh <74200917+hussein1000@users.noreply.github.com>
2025-10-22 21:06:38 +0200

    Update contact link in goTo function

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function goTo(page) {
   if (page === 'contact') {
-    window.location.href = 'https://your-contact-link.com'; // replace with your contact link
+    window.location.href = 'https://wa.me/249100099111'; // replace with your contact link
   } else if (page === 'works') {
     window.location.href = 'https://your-works-link.com'; // replace with your works link
   }
